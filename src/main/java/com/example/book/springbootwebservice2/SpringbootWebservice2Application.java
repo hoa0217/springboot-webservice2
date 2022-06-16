daac748f40a5f492d0bfe921518576aa94ac849b
허정화 <gjwjdghk123@naver.com>
2022-06-16 23:52:59 +0900

    init

---
+++ src/main/java/com/example/book/springbootwebservice2/SpringbootWebservice2Application.java
@@ -0,0 +1,13 @@
+package com.example.book.springbootwebservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book/springbootwebservice2/SpringbootWebservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book.springbootwebservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
