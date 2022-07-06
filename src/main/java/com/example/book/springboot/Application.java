b720bf6e801bced0211df81dfc7c6f8d26753ae6
허정화 <gjwjdghk123@naver.com>
2022-07-07 01:32:32 +0900

    기본테스트 (~2장)

--- src/main/java/com/example/book/springboot/Application.java
+++ src/main/java/com/example/book/springboot/Application.java
@@ -1,13 +1,13 @@
-package com.example.book.springbootwebservice2;
+package com.example.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebservice2Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebservice2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
