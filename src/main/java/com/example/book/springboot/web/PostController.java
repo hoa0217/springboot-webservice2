a15eb51bd5bf76eca9704077a0e72db6c64be9d2
허정화 <gjwjdghk123@naver.com>
2022-07-08 01:31:50 +0900

    Post Service,Controller 생성 및 Test

---
+++ src/main/java/com/example/book/springboot/web/PostController.java
@@ -0,0 +1,31 @@
+package com.example.book.springboot.web;
+
+import com.example.book.springboot.service.posts.PostService;
+import com.example.book.springboot.web.dto.PostsResponseDto;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import com.example.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postService.findById(id);
+    }
+
+}
--- src/test/java/com/example/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/example/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.book.springboot.web;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
