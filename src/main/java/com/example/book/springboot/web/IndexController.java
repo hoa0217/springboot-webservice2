866e0be87ade2c700a5b2a3d9cefb72fce87f091
hoa0217 <gjwjdghk123@naver.com>
2022-08-14 02:54:19 +0900

    get매핑 추가

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController {
 
     private final PostService postService;
 
-    @GetMapping("/")
+    @GetMapping({"","/"})
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postService.findAllDesc());
 
