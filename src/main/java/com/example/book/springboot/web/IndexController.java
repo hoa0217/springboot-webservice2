d5870ceb4faed1dbde7b69d158f8400817df0947
hoa0217 <gjwjdghk123@naver.com>
2022-08-14 01:51:34 +0900

    test 오류 들 수정 - 로컬에서는 main이 잘 실행되지만 test환경에서 안돌아감 - Innodb로 지정이 안되어 쿼리 실행X. 엔진 지정 및 db url 지정

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostService postService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
